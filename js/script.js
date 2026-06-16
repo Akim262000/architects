f73a22f8a93e0146a3da9c584413e6dc912668a5
Akim Minko <akim.minko@yandex.ru>
2026-06-16 14:57:24 +0300

    feat: add section about

--- js/script.js
+++ js/script.js
@@ -1,4 +1,13 @@
-document.querySelector(".navbar-toggler").addEventListener("click", function () {
-  this.classList.toggle("open");
-});
+document
+  .querySelector(".navbar-toggler")
+  .addEventListener("click", function () {
+    this.classList.toggle("open");
+  });
 
+$(".project-slider").slick({
+  prevArrow: ".prev-arrow",
+  nextArrow: ".next-arrow",
+  infinite: false,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+});
